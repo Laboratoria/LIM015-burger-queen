0015255710b0c4ac24fe54bc6fdbaab9a86fdc1d
mysticBel <dnxbel@gmail.com>
2021-10-21 16:50:59 -0500

    creating get-user-name in app/components to get the selector

---
+++ burguerQueen/src/app/components/get-user-name/get-user-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-user-name',
+  templateUrl: './get-user-name.component.html',
+  styleUrls: ['./get-user-name.component.css']
+})
+export class GetUserNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
